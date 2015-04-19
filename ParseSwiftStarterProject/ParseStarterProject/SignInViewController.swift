3eb5987d09d098af43edfeefa8dfa494d3cebc0b
Christine Moy <christinemoy@gmail.com>
2015-04-19 11:00:22 -0400

    initial commit2

---
+++ ParseSwiftStarterProject/ParseStarterProject/SignInViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignInViewController.swift
+//  ParseStarterProject
+//
+//  Created by Christine Moy on 4/18/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    
+}
---
+++ ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if segue.identifier == "gotosigninoptions" {
+//            var destinationVC = segue.destinationViewController as! SignInViewController
+//        }
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
